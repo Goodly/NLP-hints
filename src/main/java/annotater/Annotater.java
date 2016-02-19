20d791c1cb51e54c763e9e0ba6f19aa7c3b8b5e6
Manisha Sharma <manishasharma@Manishas-MacBook-Pro.local>
2016-02-19 15:31:27 -0800

    Added entity class

--- src/main/java/annotater/Annotater.java
+++ src/main/java/annotater/Annotater.java
@@ -3,45 +3,57 @@ package annotater;
 import org.apache.commons.io.FileUtils;
 
 
-// For XML to hashmap conversion, might use later 
-//import com.thoughtworks.xstream.XStream;
-//import com.thoughtworks.xstream.converters.Converter;
-//import com.thoughtworks.xstream.converters.MarshallingContext;
-//import com.thoughtworks.xstream.converters.UnmarshallingContext;
-//import com.thoughtworks.xstream.io.HierarchicalStreamReader;
-//import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
-//import com.thoughtworks.xstream.io.xml.DomDriver;
-
 import edu.stanford.nlp.ie.AbstractSequenceClassifier;
 import edu.stanford.nlp.ie.crf.*;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.util.Triple;
 import java.io.File;
-import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class Annotater {
+	static HashMap<String, List<Entity>> entities = new HashMap<String, List<Entity>>();
 	
-	HashMap<String, Array> entities = new HashMap<String, Array>();
-	
-	public Array getLocations() {
-		return null;
+	public Annotater() {
+		// Initialize entities map
+		entities.put("LOCATION", new ArrayList<Entity>());
+		entities.put("PERSON", new ArrayList<Entity>());
+		entities.put("ORGANIZATION", new ArrayList<Entity>());
+		
+	}
+	public void getLocations() {
+		List<Entity> locations = entities.get("LOCATION");
+		
+		for (Entity ent : locations) {
+			System.out.print("Location: " + ent.getEntity() + ", Indices: ");
+			System.out.println(ent.getIndices());
+		}
 	}
 	
-	public Array getPersons() {
-		return null;
+	public void getPersons() {
+		List<Entity> persons = entities.get("PERSON");
+		
+		for (Entity ent : persons) {
+			System.out.print("Person: " + ent.getEntity() + ", Indices: ");
+			System.out.println(ent.getIndices());
+		}
+	}
+	
+	public  void getOrgs() {
+		List<Entity> orgs = entities.get("PERSON");
+		
+		for (Entity ent : orgs) {
+			System.out.print("Orgs: " + ent.getEntity() + ", Indices: ");
+			System.out.println(ent.getIndices());
+		}
 		
 	}
 	
-	public Array getOrganizations() {
-		return null;
-		
-	}
-	public static void main(String[] args) throws Exception {
-
-	    String serializedClassifier = "ner-classifiers/english.all.3class.distsim.crf.ser.gz";
+	public void extractEntities() throws Exception {
+		String serializedClassifier = "ner-classifiers/english.all.3class.distsim.crf.ser.gz";
 
 	    AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
 	    	
@@ -55,26 +67,61 @@ public class Annotater {
 	      }
 
 	      // This gets out entities with character offsets
+	      // Add to entities hashmap
 	      int j = 0;
 	      for (String str : document) {
 	        j++;
 	        List<Triple<String,Integer,Integer>> triples = classifier.classifyToCharacterOffsets(str);
 	        for (Triple<String,Integer,Integer> trip : triples) {
-	          System.out.printf("%s over character offsets [%d, %d) in sentence %d.%n",
-	                  trip.first(), trip.second(), trip.third, j);
+	          // System.out.printf("%s over character offsets [%d, %d) in sentence %d.%n",
+	                 //  trip.first(), trip.second(), trip.third, j);
+	          List<Integer> indices = Arrays.asList(trip.second(), trip.third);
+	          Entity ent = new Entity(str.substring(trip.second(), trip.third), indices);
+	          entities.get(trip.first()).add(ent);
+	          
 	        }
-	      }
+	      } 
+		
+	}
+	
 
-//	      // This prints out all the details of what is stored for each token
-//	      int i=0;
-//	      for (String str : example) {
-//	        for (List<CoreLabel> lcl : classifier.classify(str)) {
-//	          for (CoreLabel cl : lcl) {
-//	            System.out.print(i++ + ": ");
-//	            System.out.println(cl.toShorterString());
-//	          }
-//	        }
-//	      }
+	class Entity {
+		
+		public Entity() {}
+		
+		public Entity(String entity, List<Integer> indices) {
+		       this.entity = entity;
+		       this.indices = indices;
+		    }
+		private String entity;
+		private List<Integer> indices;
+		
+		public String getEntity() { return this.entity; }
+	    public List<Integer> getIndices() { return this.indices; }
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		Annotater test = new Annotater(); 
+		test.extractEntities();
+		
+		System.out.println("Getting locations...\n-------------------");
+		test.getLocations();
+		
+		System.out.println("\nGetting Persons...\n-------------------");
+		test.getPersons();
+		
+		System.out.println("\nGetting orgs...\n-------------------");
+		test.getOrgs();
+		
+		
+
+
+	    
+	      
+
+
 
 	    }
 	  }
